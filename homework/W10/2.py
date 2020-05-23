3afde8a4c902db34739392c3fb47a4d17dc60989
AndrewDTN <xxxxx@gmail.com>
2020-05-23 18:03:24 +0800

    week10

--- homework/W10/2.py
+++ homework/W10/2.py
@@ -1,6 +1,7 @@
+#https://medium.com/pyladies-taiwan/%E7%B2%BE%E7%A2%BA%E7%9A%84%E6%B5%AE%E9%BB%9E%E6%95%B8%E9%81%8B%E7%AE%97-28d34e652e51
 from math import *
 
-step = 0.0001
+step = 0.05
 def df(f, x, h=step):
     return (f(x+h)-f(x-h))/(2*h)
 
@@ -13,3 +14,4 @@ def dfn(f, x, n, h=step):
 
 for i in range(10):
     print('dfn(sin,', i, 'pi/4)=', dfn(sin, pi/4, i))
+    
